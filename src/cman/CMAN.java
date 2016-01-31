75fee8df66ea4beb6388f34b9a43450d6c1a1a81
RandomTestFive <randomtestfive@gmail.com>
2016-01-31 16:59:25 -0500

    increment build

--- src/cman/CMAN.java
+++ src/cman/CMAN.java
@@ -37,7 +37,7 @@ import com.google.gson.JsonObject;
  */
 public class CMAN 
 {
-	int nightlyVersion = 002;
+	int nightlyVersion = 003;
 	boolean isNightly = true; 
 	String version = "1.1.0";
 	public String getVersion() { //Set devBuild to null if stable and not nightly
